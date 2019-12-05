974cdbe45765cef88e828192d144d8525eaab1a1
Gel0bmstu <leshikne@gmail.com>
2019-12-05 19:45:43 +0300

    deleted one file

--- app/src/main/java/com/example/networking/AmbitineApplication.java
+++ app/src/main/java/com/example/networking/AmbitineApplication.java
@@ -3,16 +3,6 @@ package com.example.networking;
 import android.app.Application;
 
 public class AmbitineApplication extends Application {
-    private Boolean internetIsAvailable;
-
-    public void setInternetavAilability(boolean internetAvailability) {
-        this.internetIsAvailable = internetAvailability;
-    }
-
-    public Boolean getInternetavAilability() {
-        return internetIsAvailable;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
--- app/src/main/java/com/example/networking/model/network/Picasso/ImageDownloader.java
+++ app/src/main/java/com/example/networking/model/network/Picasso/ImageDownloader.java
@@ -6,8 +6,9 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
 public class ImageDownloader {
-    public static void LoadImage(Context context, int userId, ImageView imageView) {
-        Picasso.get().load("https://cdn5.vectorstock.com/i/1000x1000/46/59/programmer-icon-in-flat-style-isolated-on-white-vector-14324659.jpg")
+    public static void LoadImage(Context context, int userId, final ImageView imageView) {
+        String url = "https://huntpng.com/images250/avatar-png-5.png";
+        Picasso.get().load(url)
                 .into(imageView);
     }
 }
