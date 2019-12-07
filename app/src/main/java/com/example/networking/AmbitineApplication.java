50cdfe0012f80273044f72ccc95013878a75f585
Temirlan <rakhimgaliev56@gmail.com>
2019-12-07 20:55:37 +0300

    picasso saves cache disk

--- app/src/main/java/com/example/networking/AmbitineApplication.java
+++ app/src/main/java/com/example/networking/AmbitineApplication.java
@@ -2,9 +2,19 @@ package com.example.networking;
 
 import android.app.Application;
 
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
 public class AmbitineApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
     }
 }
--- app/src/main/java/com/example/networking/model/network/Picasso/ImageDownloader.java
+++ app/src/main/java/com/example/networking/model/network/Picasso/ImageDownloader.java
@@ -8,7 +8,8 @@ import com.squareup.picasso.Picasso;
 public class ImageDownloader {
     public static void LoadImage(Context context, int userId, final ImageView imageView) {
         String url = "https://huntpng.com/images250/avatar-png-5.png";
-        Picasso.get().load(url)
+        Picasso.get()
+                .load(url)
                 .into(imageView);
     }
 }
