c76f76fafea226b6222876cb45a9d4e0d2d26ce0
evvfebruary <Firebulletred14Yn>
2019-12-22 02:41:07 +0300

    # Export promises

--- app/src/main/java/com/example/networking/model/network/Retrofit/FeedService.java
+++ app/src/main/java/com/example/networking/model/network/Retrofit/FeedService.java
@@ -9,6 +9,6 @@ import retrofit2.http.GET;
 
 public interface FeedService {
 //    http://35.228.98.103:9090/api/get_promises
-    @GET("api/get_promises/")
+    @GET("api/get_export_promises/")
     Call<List<Promise>> getAllFeedItems();
 }
