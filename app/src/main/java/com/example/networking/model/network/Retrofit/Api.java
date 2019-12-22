4a38646c869cfcddb6a58fedb912812c5fc1cb0a
evvfebruary <Firebulletred14Yn>
2019-12-22 13:53:49 +0300

    # Prod url

--- app/src/main/java/com/example/networking/model/network/Retrofit/Api.java
+++ app/src/main/java/com/example/networking/model/network/Retrofit/Api.java
@@ -1,8 +1,10 @@
 package com.example.networking.model.network.Retrofit;
 
 public class Api {
-//    public static final String BASE_URL = "http://10.0.2.2:3000/";
-    public static final String BASE_URL = "http://192.168.100.32:9090/";
+////    public static final String BASE_URL = "http://10.0.2.2:3000/";
+//    public static final String BASE_URL = "http://192.168.100.32:9090/";
+    public static final String BASE_URL = "http://35.228.98.103:9090/";
+//    .baseUrl("http://35.228.98.103:9090/")
 //    public static final String BASE_URL = "http://www.mocky.io/";
 
     public static ApiService getApiService(){
