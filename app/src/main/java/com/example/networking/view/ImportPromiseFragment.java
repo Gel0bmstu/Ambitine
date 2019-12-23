586f4f22220c981430d6619525171c5596e66607
evvfebruary <Firebulletred14Yn>
2019-12-23 18:43:57 +0300

    # Import promise logic to controller

--- app/src/main/java/com/example/networking/view/ImportPromiseFragment.java
+++ app/src/main/java/com/example/networking/view/ImportPromiseFragment.java
@@ -1,50 +1,18 @@
 package com.example.networking.view;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.networking.R;
 import com.example.networking.conroller.ImportPromiseController;
-import com.example.networking.model.models.Promise;
-import com.example.networking.model.network.Retrofit.Api;
-import com.example.networking.model.network.Retrofit.ApiService;
-import com.example.networking.model.network.Retrofit.ExportPromiseService;
-import com.example.networking.model.network.Retrofit.Interceptors.AddCookiesInterceptor;
-import com.example.networking.model.network.Retrofit.Interceptors.ReceivedCookiesInterceptor;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import static com.example.networking.model.network.Retrofit.Api.BASE_URL;
 
 public class ImportPromiseFragment extends Fragment {
     private ImportPromiseController importPromiseController;
-    private RecyclerView.Adapter promiseAdapter;
-    private RecyclerView.LayoutManager promeseLayoutManager;
-    View rootView;
-    private List<Promise> gettedPromises = new ArrayList<>();
+    private View rootView;
 
 
     @Nullable
@@ -56,107 +24,15 @@ public class ImportPromiseFragment extends Fragment {
 
         super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.fragment_import_promise_feed, container, false);
-        getFeedData();
-//        rootView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-//            @Override
-//            public void onScrollChanged() {
-//                if (rootView != null) {
-//                    if (rootView.getScrollY() == 0) {
-//                        Toast.makeText(getContext(), "top", Toast.LENGTH_SHORT).show();
-//                    } else {
-//
-//                    }
-//                }
-//            }
-//        });
+        importPromiseController.getFeedData();
         return rootView;
-//        return super.onCreateView(inflater, containeer, savedInstanceState);
     }
 
-
-    Gson gson = new GsonBuilder().create();
-    OkHttpClient client = new OkHttpClient.Builder().
-            addInterceptor(new AddCookiesInterceptor()).
-            addInterceptor(new ReceivedCookiesInterceptor()).
-            build();
-
-    // use retrofit to create an instance of BookService
-    ExportPromiseService service = new Retrofit.Builder()
-//            .baseUrl("http://www.mocky.io/")
-//            .baseUrl("http://192.168.100.32:9090")
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(client)
-            .build()
-            .create(ExportPromiseService.class);
-
-    public void getFeedData() {
-        ApiService apiService = Api.getApiService();
-        Call<List<Promise>> call = service.getAllImportPromises();
-        System.out.println("WE INB");
-        call.enqueue(new Callback<List<Promise>>() {
-            @Override
-            public void onResponse(Call<List<Promise>> call, Response<List<Promise>> response) {
-                System.out.println("WATA SHAKA LAKA");
-                if (response.code() == 200) {
-                    assert response.body() != null;
-                    System.out.println("WATA SHAKA LAKA");
-                    RelativeLayout feedLayout = rootView.findViewById(R.id.import_promise_layout);
-                    TextView promisesNotFound = rootView.findViewById(R.id.not_promises);
-                    if (promisesNotFound != null) {
-                        feedLayout.removeView(promisesNotFound);
-                    }
-                    // Tmp method to get data
-                    RecyclerView recyclerView = rootView.findViewById(R.id.import_promise_feed);
-
-
-                    recyclerView.setHasFixedSize(true);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-
-                    // feedController.setFeedData();
-//                    feedController.setFeedData();
-                    String[] myStringArray;
-                    List<Promise> promises = response.body();
-                    PromiseImportAdapter mAdapter = new PromiseImportAdapter(promises);
-                    // 4. set adapter
-                    recyclerView.setAdapter(mAdapter);
-                    // 5. set item animator to DefaultAnimator
-                    recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-                } else if (response.code() == 404) {
-                    TextView promisesNotFound = new TextView(rootView.getContext());
-                    promisesNotFound.setText("You have no promises so far.");
-                    promisesNotFound.setId(R.id.not_promises);
-                    // Dont work (
-                    promisesNotFound.setGravity(Gravity.CENTER_HORIZONTAL);
-                    promisesNotFound.setTextSize(25);
-                    promisesNotFound.setTextColor(getResources().getColor(R.color.ambitine_primary_color));
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(0,350,0,0);
-                    promisesNotFound.setLayoutParams(params);
-                    RelativeLayout feedLayout = rootView.findViewById(R.id.import_promise_layout);
-                    // ToDo: Maybe add image
-                    if (rootView.findViewById(R.id.not_promises) == null) {
-                        feedLayout.addView(promisesNotFound);
-                    }
-                } else {
-                    System.out.println("Another handle way");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Promise>> call, Throwable t) {
-                System.out.println("FOCK");
-                System.out.println(t.toString());
-            }
-        });
-    }
 
     @Override
     public void onHiddenChanged(boolean hidden) {
         if (!hidden) {
-            getFeedData();
+            importPromiseController.getFeedData();
         }
         super.onHiddenChanged(hidden);
     }
