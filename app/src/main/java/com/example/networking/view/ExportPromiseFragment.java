d5d90273ff12d0a46da05da048fa3c32e0bcaf91
evvfebruary <Firebulletred14Yn>
2019-12-23 19:15:04 +0300

    # Remove warning

--- app/src/main/java/com/example/networking/view/ExportPromiseFragment.java
+++ app/src/main/java/com/example/networking/view/ExportPromiseFragment.java
@@ -4,19 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.networking.R;
 import com.example.networking.conroller.ExportPromiseController;
-import com.example.networking.model.models.Promise;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class ExportPromiseFragment extends Fragment {
