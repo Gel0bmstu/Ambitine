567b17652ad581776696f64d64db10bbf1ef35cc
evvfebruary <Firebulletred14Yn>
2019-12-26 01:04:25 +0300

    # Timestamp checker

--- app/src/main/java/com/example/networking/controller/NewPromiseController.java
+++ app/src/main/java/com/example/networking/controller/NewPromiseController.java
@@ -1,11 +1,16 @@
 package com.example.networking.controller;
 
+import android.widget.Toast;
+
 import com.example.networking.model.network.Retrofit.Api;
 import com.example.networking.model.network.Retrofit.ApiService;
 import com.example.networking.model.network.Retrofit.Response.NewPromiseResponce;
 import com.example.networking.view.PromiseCreaterFragment;
 
 import org.jetbrains.annotations.NotNull;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.util.Objects;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -20,14 +25,28 @@ public class NewPromiseController {
         this.newPromiseFragment = promiseFragment;
     }
 
+    public Long getNowTimestamp() {
+        Date date= new Date();
+        return date.getTime();
+    }
+
 
     public void onNewPromiseButtonClick() {
         String username = newPromiseFragment.getRecieverUsername();
         Integer deposit = newPromiseFragment.getDeposit();
         String description = newPromiseFragment.getPromiseDescription();
         Long pastdue = newPromiseFragment.getPastDue();
-        NewPromiseResponce newPromiseReponce = new NewPromiseResponce(username, description, pastdue, deposit);
-        sendNewPromise(newPromiseReponce);
+        // Date checker
+
+        int compared = pastdue.compareTo(getNowTimestamp());
+        if (compared > 0  || compared == 0) {
+            Toast.makeText(Objects.requireNonNull(newPromiseFragment.getActivity())
+                    .getApplicationContext(),"Available only if you are timetraveller!",Toast.LENGTH_SHORT).show();
+        } else {
+            NewPromiseResponce newPromiseReponce = new NewPromiseResponce(username, description, pastdue, deposit);
+            sendNewPromise(newPromiseReponce);
+        }
+
     }
 
     private void sendNewPromise(final NewPromiseResponce newPromiseResponce) {
@@ -45,8 +64,7 @@ public class NewPromiseController {
             }
             @Override
             public void onFailure(@NotNull Call<ResponseBody> call, @NotNull Throwable t) {
-                System.out.println("Send new promise failure");
-                System.out.println(t.toString());
+                Toast.makeText(Objects.requireNonNull(newPromiseFragment.getActivity()).getApplicationContext(),"New promise create failed",Toast.LENGTH_SHORT).show();
             }
         });
     }
