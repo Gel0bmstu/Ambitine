980b16e05d2e4cf0973fe445e79e7547b468c651
Temirlan <rakhimgaliev56@gmail.com>
2020-04-02 18:48:12 +0300

    gradle change

--- app/src/main/java/com/example/networking/model/network/Retrofit/Interceptors/AddCookiesInterceptor.java
+++ app/src/main/java/com/example/networking/model/network/Retrofit/Interceptors/AddCookiesInterceptor.java
@@ -1,9 +1,9 @@
 package com.example.networking.model.network.Retrofit.Interceptors;
 
-import android.util.Log;
-
 import com.example.networking.model.UserRepository;
 import com.example.networking.model.database.UserDB;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -12,12 +12,11 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class AddCookiesInterceptor implements Interceptor {
+    @NotNull
     @Override
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Request.Builder builder = chain.request().newBuilder();
         String authToken = UserRepository.getToken();
-        Log.d("hellomelloy4 hui ", authToken);
-        Log.d("hellomelloy4 hui ", UserRepository.getToken());
         if (!authToken.equals(UserDB.TOKEN_NOT_FOUND)) {
             builder.addHeader("Cookie", authToken);
         }
